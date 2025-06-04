97dc13c126bf7a0d8fc2a4250534395de98bd672
Richmond014 <racelisrichmond@gmail.com>
2025-06-04 14:24:10 +0800

    "ily von"

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import '../styles/Navbar.css';
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../styles/Navbar.css";
 
 const About = () => {
   return (
-    <div>
-      <Navbar/>
-      <h2>About Page</h2>
+    <div style={{ backgroundColor: "red", padding: "20px", marginTop: "50px" }}>
+      <Navbar />
+      <h2 style={{ color: "white" }}>Request Page</h2>
+      <p style={{ color: "white" }}>
+        If you see this, the component is working!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/pages/Request.jsx
+++ src/pages/Request.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Request = () => {
-  return (
-    <div>
-      <Navbar/>
-      <h2>Request Page</h2>
-    </div>
-  )
-}
+  console.log("Request component is rendering!");
 
-export default Request
+  return (
+    <div style={{ backgroundColor: "red", padding: "20px", marginTop: "50px" }}>
+      <Navbar />
+      <h2 style={{ color: "white" }}>Request Page</h2>
+      <p style={{ color: "white" }}>
+        If you see this, the component is working!
+      </p>
+    </div>
+  );
+};
+
+export default Request;
