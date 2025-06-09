89e151059d0a912291b099a555030e1949d141a8
Florence <cansinoflorencelee44@gmail.com>
2025-06-09 23:15:28 +0800

    modified navbar jsx css and added sidebardata jsx

---
+++ src/components/Sidebardata.jsx
@@ -0,0 +1,23 @@
+import * as IoIcons from 'react-icons/io5';
+
+export const SidebarData = [
+
+  {
+    title: 'Dashboard',
+    path: '/dashboard',
+    icon: <IoIcons.IoHomeOutline />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'Create New Request',
+    path: '/request',
+    icon: <IoIcons.IoAddCircleOutline />,
+    cName: 'nav-text',
+  },
+  {
+    title: 'FAQ/Help',
+    path: '/help',
+    icon: <IoIcons.IoHelpCircleOutline />,
+    cName: 'nav-text',
+  }
+];
